34d11890f36085a17f65cb794ee7e48d67ae695c
Ibrahim Faruk İnce <ibrahimfarukince@gmail.com>
2022-07-02 02:19:58 +0300

    first commit

---
+++ smartedu_con/pages/views.py
@@ -0,0 +1,8 @@
+import imp
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("<h1>Index Sayfası</h1>")
+
+# Create your views here.
